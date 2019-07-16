006568b0c1c31d072d178870b34ca1d4521c15ba
MCdirtyjob <34293804+MCdirtyjob@users.noreply.github.com>
2019-07-16 10:57:19 -0600

    Change State Values

--- React-CTS-PR1/src/components/portfolio/portfolio-container.js
+++ React-CTS-PR1/src/components/portfolio/portfolio-container.js
@@ -5,23 +5,42 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component{
     constructor(){
         super();
-        console.log('Portfolio container has rendered');
+
+        this.state ={
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: 'Quip'},
+                {title: 'Eventbrite'},
+                {title: 'Ministry Safe'},
+                {title: "SwingAway"}
+            ]
+        };
+
+        this.handlePageTitleUpdate= this.handlePageTitleUpdate.bind(this);
     }
 
     portfolioItems(){
-        const data=['Quip', 'Eventbrite', 'Ministry Safe'];
-
-        return data.map(item => {
-            return <PortfolioItem title={item} url={"google.com"}/>;
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"}/>;
         })
+    }
+
+    handlePageTitleUpdate(){
+        this.setState({
+            pageTitle: "Something Else"
+        });
     }
 
     render(){
         return(
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
+
+                <hr/>
+
+                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
