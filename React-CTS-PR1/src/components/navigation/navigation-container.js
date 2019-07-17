8b9fc701d3c035c0c60f67d4caae19a61fc17f12
MCdirtyjob <34293804+MCdirtyjob@users.noreply.github.com>
2019-07-17 11:12:15 -0600

    Basic Route setup

--- React-CTS-PR1/src/components/navigation/navigation-container.js
+++ React-CTS-PR1/src/components/navigation/navigation-container.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class NavigationComponent extends Component{
-    constructor(){
-        super();
-    }
+export default class NavigationComponent extends Component {
+  constructor() {
+    super();
+  }
 
-    render(){
-        return(
-            <div>
-                <button>Home</button>
-                <button>About</button>
-                <button>Contact</button>
-                <button>Blog</button>
-                {true ? <button>Add Blog</button> : null}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <button>Home</button>
+        <button>About</button>
+        <button>Contact</button>
+        <button>Blog</button>
+        {true ? <button>Add Blog</button> : null}
+      </div>
+    );
+  }
 }
