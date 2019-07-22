7660bc5fd566d75c2e7eef36d12860c3b335fe37
MCdirtyjob <34293804+MCdirtyjob@users.noreply.github.com>
2019-07-22 11:56:04 -0600

    W.I.P Auth

--- React-CTS-PR1/src/components/pages/auth.js
+++ React-CTS-PR1/src/components/pages/auth.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 export default class Auth extends Component {
   render() {
-    return <div>Auth....</div>;
+    return (
+      <div className="auth-page-wrapper">
+        <div className="left-column" />
+        <div className="right-column">
+          <h1>Left</h1>
+        </div>
+      </div>
+    );
   }
 }
