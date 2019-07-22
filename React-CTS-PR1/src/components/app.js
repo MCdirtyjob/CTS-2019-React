b1b127025555319da374b15197cfcb9b8eb1da08
MCdirtyjob <34293804+MCdirtyjob@users.noreply.github.com>
2019-07-22 11:47:10 -0600

    Button stlye/Auth

--- React-CTS-PR1/src/components/app.js
+++ React-CTS-PR1/src/components/app.js
@@ -7,7 +7,8 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio.detail";
-import NoMatch from "./pages/no-match.js";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -19,6 +20,7 @@ export default class App extends Component {
 
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
