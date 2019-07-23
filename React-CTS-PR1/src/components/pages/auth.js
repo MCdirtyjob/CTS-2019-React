a92fa0d20b9e53e0b17f0e414308f6355b49c3b7
MCdirtyjob <34293804+MCdirtyjob@users.noreply.github.com>
2019-07-23 08:13:07 -0600

    Static Images Import

--- React-CTS-PR1/src/components/pages/auth.js
+++ React-CTS-PR1/src/components/pages/auth.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
   render() {
     return (
       <div className="auth-page-wrapper">
-        <div className="left-column" />
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
+
         <div className="right-column">
-          <h1>Left</h1>
+          <h1>Login component goes here...</h1>
         </div>
       </div>
     );
