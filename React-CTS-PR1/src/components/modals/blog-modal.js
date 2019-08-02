33d4d135c8522791502f41e3dba856fc6be5a532
MCdirtyjob <34293804+MCdirtyjob@users.noreply.github.com>
2019-08-02 10:58:10 -0600

    Blog Modal Submit

--- React-CTS-PR1/src/components/modals/blog-modal.js
+++ React-CTS-PR1/src/components/modals/blog-modal.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
 
+import BlogForm from "../blog/blog-form";
+
 ReactModal.setAppElement(".app-wrapper");
 
 export default class BlogModal extends Component {
@@ -20,6 +22,14 @@ export default class BlogModal extends Component {
         backgroundColor: "rgba(1, 1, 1, 0.75)"
       }
     };
+
+    this.handleSuccessfullFormSubmission = this.handleSuccessfullFormSubmission.bind(
+      this
+    );
+  }
+
+  handleSuccessfullFormSubmission(blog) {
+    console.log("blog from blog form", blog);
   }
 
   render() {
@@ -31,7 +41,9 @@ export default class BlogModal extends Component {
         }}
         isOpen={this.props.modalIsOpen}
       >
-        <h1>I'm in a modal!</h1>
+        <BlogForm
+          handleSuccessfullFormSubmission={this.handleSuccessfullFormSubmission}
+        />
       </ReactModal>
     );
   }
